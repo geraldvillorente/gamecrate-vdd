202021678672479b2a8a5cda1c78954da99a2197
Gerald Villorente <geraldvillorente@gmail.com>
2014-10-14 14:23:13 +0800

    Added configurations for gamecrate vdd

---
+++ manifests/site.pp
@@ -0,0 +1,12 @@
+# DO NOT DELETE THIS LINE!
+include undine
+
+# Override environment
+include drupalvdd_env
+
+# Include your Undine sites below. These names will correspond to those
+# subdirectories in your ./sites directory, and their class names as declared in
+# ./sites/YOURSITENAME/manifests/init.pp
+
+# Include Gamecrate site
+include gamecrate
